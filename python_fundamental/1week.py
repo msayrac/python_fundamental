fd8a2932c276c741d70ba73e62196c63ab22b031
Muhammed Sayrac <94198566+msayrac@users.noreply.github.com>
2025-07-02 16:04:39 +0300

    Add files via upload

---
+++ python_fundamental/1week.py
@@ -0,0 +1,43 @@
+# Numbers
+print('2')
+print(type('2'))
+print(type(3))
+print(type(3.8))
+
+#operators
+
+print(2+2)
+print(2-2)
+print(2*2)
+print(2/2)
+print(2**2)
+print(5%2)
+print(10//3) # takes integer part of the division
+print(5**2)
+
+print(10//4) # whole division
+
+# variables
+
+urunA =5000
+urunB = 6000
+urunC = 7000
+
+kdv = 0.18
+
+print(urunA+ (urunA*kdv))
+print(urunB+ (urunB*kdv))
+print(urunC+ (urunC*kdv))
+print("******KDV increased***********")
+kdv = 0.2
+print(urunA+ (urunA*kdv))
+print(urunB+ (urunB*kdv))
+print(urunC+ (urunC*kdv))
+
+
+
+
+
+
+
+
