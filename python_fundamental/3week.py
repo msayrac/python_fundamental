3e914259f396cc99bf3185cb09ef09f499ea0cb2
Muhammed Sayrac <94198566+msayrac@users.noreply.github.com>
2025-07-04 15:38:24 +0300

    Add files via upload

---
+++ python_fundamental/3week.py
@@ -0,0 +1,24 @@
+adi = "Kemal"
+soyad = "Sunal"
+yas = "37"
+
+msj = "Benim adım "+ adi +" ve soyadım "+soyad+". Yaşım ise "+ yas +"."
+
+print(msj)
+print(msj[0])
+
+print(msj[-1]) # son karakter
+print(msj[-3]) # -3 üncu karakter
+
+
+print(msj[0:5]) # birden fazla karakter. 
+
+print(msj[0:6:2]) # start : stop: step
+
+print(msj[:5]) # baslangıc karakterini yazmaya gerek yok. bastan baslatmak icin
+print(msj[5:]) # sona kadar alır
+
+print(msj[::-1]) # saymaya sondan baslar
+
+
+
